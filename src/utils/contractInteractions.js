774e199a8805f4b4510eab138b9d3c4d51c6d429
Aakriti mehrotra <aakriti.meh.2004@gmail.com>
2024-07-28 18:35:56 +0530

    added update and enroll member to contractInteractions

--- src/utils/contractInteractions.js
+++ src/utils/contractInteractions.js
@@ -1,8 +1,8 @@
-import { ethers } from 'ethers';
-import Database from '../../blockchain/artifacts/contracts/Database.sol/Database.json' assert { type: 'json' };
+import { ethers } from "ethers";
+import Database from "../../blockchain/artifacts/contracts/Database.sol/Database.json" assert { type: "json" };
 
-const contractAddress = '0x80bff3474C93c1eA07081cEF6bb87Bc1626ae949';
-const rpcUrl = 'https://rpc-amoy.polygon.technology/'; 
+const contractAddress = "0x80bff3474C93c1eA07081cEF6bb87Bc1626ae949";
+const rpcUrl = "https://rpc-amoy.polygon.technology/";
 
 function getProvider() {
   return new ethers.providers.JsonRpcProvider(rpcUrl);
@@ -30,15 +30,65 @@ export async function isEnrolled(address) {
   return await contract.isEnrolled(address);
 }
 
-function timeConverter(UNIX_timestamp){
+export async function enrollMember(_name, _email, _phone, _regno, _msg) {
+  const contract = await getContract();
+  const enrollWithUs = await contract.enroll(
+    _name,
+    _email,
+    _phone,
+    _regno,
+    _msg
+  );
+  if (enrollWithUs) {
+    alert("Congrats!! you are a part of DAO now");
+  } else {
+    alert("sorry we couldn't register you; try again.");
+  }
+}
+
+export async function updateMemInfo(_name, _email, _phoneNumber, _msg) {
+  const contract = await getContract();
+  const modifyInfo = await contract.enroll(_name, _email, _phone, _msg);
+  if (modifyInfo) {
+    alert("Modified Successfull");
+  } else {
+    alert("Could Not Modify the Data");
+  }
+}
+
+function timeConverter(UNIX_timestamp) {
   var a = new Date(UNIX_timestamp * 1000);
-  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
   var year = a.getFullYear();
   var month = months[a.getMonth()];
   var date = a.getDate();
-  var hour = "0"+a.getHours();
-  var min = "0"+ a.getMinutes();
-  var sec = "0"+ a.getSeconds();
-  var time = date + ' ' + month + ' ' + year + ' - ' + hour.substr(-2) + ':' + min.substr(-2) + ':' + sec.substr(-2) ;
+  var hour = "0" + a.getHours();
+  var min = "0" + a.getMinutes();
+  var sec = "0" + a.getSeconds();
+  var time =
+    date +
+    " " +
+    month +
+    " " +
+    year +
+    " - " +
+    hour.substr(-2) +
+    ":" +
+    min.substr(-2) +
+    ":" +
+    sec.substr(-2);
   return time;
 }
